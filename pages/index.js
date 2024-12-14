14d7935aede6b728a689c5587027d56f5a680540
mirrormouse <kagamidokei@gmail.com>
2024-12-14 21:40:15 +0900

    build

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div style={{ margin: '20px' }}>
       <h1>振り分けアプリ</h1>
-      <Link href="/new">新しい振り分けを作成する</Link>
+      <Link href="/new">新しい振り分けの作成</Link>
     </div>
   );
 }
